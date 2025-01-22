2993e231c16c8f7e9e6f1668fe508ccbcbb6c591
hebib57 <d-del57000@hotmail.fr>
2025-01-22 09:35:58 +0100

    Affichage additionnel

--- exercices.js
+++ exercices.js
@@ -2,3 +2,12 @@ const nom_boutique = "shop";
 let nb_potions_stock = 6;
 let prix_potion = 5;
 let mag_statut = true;
+
+//Affichage additionnel
+if (mag_statut == true) {
+  console.log("`Bienvenue dans la boutique shop Aventurier ! 🎉`");
+} else {
+  console.log(
+    "`La boutique shop est fermée, revenez plus tard Aventurier ! 😴`"
+  );
+}
